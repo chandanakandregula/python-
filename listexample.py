b8d597ee10765f5a265bc1359ea7ee5458a401b1
chandanakandregula <chandanakandregula4@gmail.com>
2026-07-18 14:03:34 +0530

    Add files via upload

---
+++ listexample.py
@@ -0,0 +1,40 @@
+li=['java','python','c','c++']
+print(li)
+print(li[1])
+print(li[-2])
+print(li)
+li.append('java script')
+print(li)
+li.insert(4,'HTMl')
+print(li)
+del li[2]
+print(li)
+del li[-3]
+print(li)
+popped_element=li.pop()
+print(popped_element)
+print(li)
+li.remove('HTMl')
+print(li)
+li.append('c')
+print(li)
+li.sort()
+print(li)
+li.sort(reverse=True)
+print(li)
+li.reverse
+print(li)
+print(len(li))
+for lang in li:
+    print(lang.upper())
+l3=list(range(1,10))
+print(l3)
+print(min(l3))
+print(max(l3))
+print(sum(l3))
+
+#sclicing a list
+print(l3[0:4])
+print(l3[4:9])
+print(l3[:5])
+
