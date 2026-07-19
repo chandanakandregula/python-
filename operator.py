55478c993973f3063eb56fff41d83cc9b6938291
chandanakandregula <chandanakandregula4@gmail.com>
2026-07-19 10:41:22 +0530

    Add files via upload

--- operator.py
+++ operator.py
@@ -1,10 +1,57 @@
 a=5
 b=10
+#arthemetic operators
 #print(a+b)
 #print(a-b)
 #print(a*b)
 print(a/b)
-print(a%b)
-print(a//b)
+print(a%b) #modulus division
+print(a//b)  #floor division
 print(a**b)
+
+
+#comparision operators
+print(a<b)
+print(a>b)
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print("-------------")
+
+
+
+#logical operators
+num1=5
+num2=10
+print(num1*num2)
+print(num1>5 and num2>5)
+print(num1>5 or num2>5)
+print(not(num1>num2))
+print("---------")
+
+
+#assignment operstors
+num1=num1+num2
+print(num1)
+num1=num1==num2
+print(num2)
+num1=num1!=num2
+print(num1)
+num1=num1-num2
+print(num1)
+num1=num1%num2
+print(num2)
+print(("---------"))
+
+#membership operators
+print("a" in "apple")
+print("a" not in "apple")
+print(("---------"))
+
+
+#identity operator
+name1="apple"
+name2="apple"
+print(name1 is not name2)
 
